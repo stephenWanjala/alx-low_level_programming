e1899fadbebdab96e188de036e892c3a5d4eab6e
stephen wanjala <stephenwanjala145@gmail.com>
2022-09-14 11:05:35 +0300

    0-putchaar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,8 +12,8 @@ int main(void)
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
-		_putchar(str[count]);
+		putchar(str[count]);
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
