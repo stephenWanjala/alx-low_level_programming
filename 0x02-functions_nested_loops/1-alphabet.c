6cb547eec732fbd5634ce8a46c75713c1c447bb8
stephen wanjala <stephenwanjala145@gmail.com>
2022-09-14 14:34:03 +0300

    1&2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet - Entry point
  * Description: a function that prints the alphabet,
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-#include "1-alphabet.c"
-#include "2-print_alphabet_x10.c"
+
